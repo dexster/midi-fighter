f9ebbf78748508b117309c0ed69f975b15d167ad
dexster <>
2024-04-28 12:15:33 +0200

    publish config

--- forge.config.js
+++ forge.config.js
@@ -24,6 +24,18 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'dexster',
+          name: 'midi-fighter-rematch'
+        },
+        prerelease: true
+      }
+    }
+  ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
